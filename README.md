3d1b32ada2454ece6dc96f5a117c5f2d755e0110
sanscomp <sanscomp@anslems-iMac.local>
2019-05-15 23:36:30 +0100

    Project setup/scaffolding

---
+++ README.md
@@ -0,0 +1,27 @@
+# Countries of the World app with colour theme switcher
+
+![desktop-preview](https://user-images.githubusercontent.com/14879253/57810834-a9bfdd00-7760-11e9-8017-ea02c1fe8424.jpg)
+
+Build an app similar to this challenge on [frontendmentor](https://beta.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca).
+
+## Brief:
+**Use the REST countries API to pull and display country data, enabling users to:**
+
+ * See all countries from the API on the homepage
+ * Search for a country using an input field
+ * Filter countries by region
+ * Click on a country to see more detailed information on a  separate page
+ * Click through to the border countries on the detail page
+ * Toggle the color scheme between light and dark mode (optional)
+
+## Built from scratch with
+* Gulp
+* Sass
+* HTML5 
+* Javascript 
+* Flexbox
+* Grid
+* REST Countries API
+
+### License:
+MIT 2019
---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
+const uglify = require("gulp-uglify-es").default;
+const imagemin = require("gulp-imagemin");
+const { series, parallel } = require("gulp");
+
+//  -- Functions --
+//  src - points to files
+//  dest - points to output folder 
+
+
+// Copy html files
+function copyHtml() {
+  return gulp
+    .src("src/*.html").pipe(gulp.dest("dist"));
+};
+
+//Copy & minify javascript files
+function minifyJs() {
+  return gulp
+    .src("src/js/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/js"));
+};
+
+//  Optimise images
+function imageOptim() {
+  return gulp
+    .src("src/images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/images"));
+};
+
+// Compile Sass & autoprefix
+function compileSass() {
+  return gulp
+    .src("src/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer({ browsers: ["last 2 versions"], cascade: false }))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.stream());
+};
+
+//  Watch for changes & reload browser
+function watch() {
+  browserSync.init({ server: "./dist" });
+  gulp.watch("src/sass/**/*.scss", compileSass);
+  gulp.watch("src/*.html", copyHtml).on("change", browserSync.reload);
+  gulp.watch("src/js/**/*.js", minifyJs).on("change", browserSync.reload);
+};
+
+exports.default = series(watch);
+exports.build = parallel(copyHtml, compileSass, imageOptim, minifyJs, watch);
