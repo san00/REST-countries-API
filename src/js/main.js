b45cd5c2bc5d7736cac994c043e0c0032a02043e
sanscomp <sanscomp@anslems-iMac.local>
2019-05-25 21:10:00 +0100

    Add display function

--- src/js/main.js
+++ src/js/main.js
@@ -1,42 +1,37 @@
+const url = "https://restcountries.eu/rest/v2/all";
+const countries = [];
+
+fetch(url)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        countries.push(...data)
+        return countries;
+
+    }).then(displayCountries);
+
+
 function displayCountries() {
-
     const card = document.querySelector(".card");
-    
-    fetch('https://restcountries.eu/rest/v2/all')
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-          
-            data.forEach(country => {
+      countries.forEach(country => {
 
-                //create container for each country
-                let div = document.createElement("div")
-                div.setAttribute("class", "card__container")
-                
-                //append country flag
-                let img = document.createElement('img')
-                img.setAttribute("class", "card__img")
-                img.src = country.flag
-                div.append(img)
-                card.append(div);
-                
-                //append country name 
-                let countryName = document.createElement('h3')
-                countryName.setAttribute("class", "card__name")
-                countryName.append(country.name)
-                div.append(countryName);
-                card.append(div);
-                
-                //append country information
-                let info = document.createElement('h4')
-                info.setAttribute("class", "card__info")
-                info.append(`Population: ${country.population}`);
-                div.append(info);
-                card.append(div);
+        //create container for each country
+        let div = document.createElement("div")
+        div.setAttribute("class", "card__container")
+        
+        //append flag
+        let img = document.createElement('img')
+        img.setAttribute("class", "card__img")
+        img.src = country.flag;
+        div.append(img)
+        card.append(div);
 
-            });
-        })
-};
-
-displayCountries();
+        //append name 
+        let countryName = document.createElement('h3')
+        countryName.setAttribute("class", "card__name")
+        countryName.append(country.name)
+        div.append(countryName);
+        card.append(div);
+    })
+}
