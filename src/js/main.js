1779cbe1bfd93280f3f8b4586318931f852ade73
sanscomp <sanscomp@anslems-iMac.local>
2019-05-31 15:18:35 +0100

    Add search function

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,62 @@
 const url = "https://restcountries.eu/rest/v2/all";
 const countries = [];
+let searchedCountry;
+const input = document.querySelector(".searchbar__input");
+input.addEventListener("keyup", updateSearchedCountry);
 
 fetch(url)
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        countries.push(...data)
-        return countries;
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    countries.push(...data);
+    // console.log(countries);
+    return countries;
+  })
+  .then(countries => {
+    console.log(searchedCountry);
+    return matchCountry(searchedCountry, countries);
+  })
+  .then(matchedCountry => {
+    return displayCountries(matchedCountry);
+  })
+  .catch(error => console.error(error));
 
-    }).then(displayCountries);
 
+function updateSearchedCountry() {
+    //updates search box value
+  searchedCountry = input.value;
+  return searchedCountry;
+}
 
-function displayCountries() {
-    const card = document.querySelector(".card");
-      countries.forEach(country => {
+function matchCountry(searchedCountry, countries) {
+  const matchedCountry = countries.filter(place => {
+    //check if search term matches any countries
+    const regex = new RegExp(searchedCountry, "gi"); //global match, ignore case
+    return place.name.match(regex) && place.flag.match(regex);
+  });
+  return matchedCountry;
+}
 
-        //create container for each country
-        let div = document.createElement("div")
-        div.setAttribute("class", "card__container")
-        
-        //append flag
-        let img = document.createElement('img')
-        img.setAttribute("class", "card__img")
-        img.src = country.flag;
-        div.append(img)
-        card.append(div);
+function displayCountries(matchedCountry) {
+  const card = document.querySelector(".card");
+  matchedCountry.forEach(country => {
+    //create container for each country
+    let div = document.createElement("div");
+    div.setAttribute("class", "card__container");
 
-        //append name 
-        let countryName = document.createElement('h3')
-        countryName.setAttribute("class", "card__name")
-        countryName.append(country.name)
-        div.append(countryName);
-        card.append(div);
-    })
+    //append flag
+    let img = document.createElement("img");
+    img.setAttribute("class", "card__img");
+    img.src = country.flag;
+    div.append(img);
+    card.append(div);
+
+    //append name
+    let countryName = document.createElement("h3");
+    countryName.setAttribute("class", "card__name");
+    countryName.append(country.name);
+    div.append(countryName);
+    card.append(div);
+  });
 }
